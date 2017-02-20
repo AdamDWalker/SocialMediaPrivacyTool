ff033b596920b7aee2c8990377fe58af2bfad1a3
AdamDWalker <adamwalker03@googlemail.com>
2017-02-20 15:19:35 +0000

    Minor formatting changes

--- PrivacyTool.py
+++ PrivacyTool.py
@@ -1,6 +1,5 @@
 import tweepy
 
-
 # Consumer keys and access tokens, used for OAuth
 consumer_key = 'YUOKTebHRQH1MUv3ZlIZ3SKM3'
 consumer_secret = 'FZJtpidnF24hDL6wgTAf2Tfqa8lJj8ZaVzWLbzOAdAty7cxDFe'
@@ -14,12 +13,11 @@ auth.set_access_token(access_token, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
-# Retrieve tweets from the user_timeline
-stuff = api.user_timeline(screen_name = 'adamdwalker', count = 100, include_rts = True)
+# Retrieve stuff (Everything basically) from the user_timeline
+stuff = api.user_timeline(screen_name = 'tombarden', count = 100, include_rts = True)
 
 for tweet in stuff:
-    print ("Tweet: " + tweet.text + " --- Time: " + tweet.created_at.strftime('%d/%m/%y -- %H:%M'))
-
+    print ("|Tweet| - " + tweet.text + "   ==  |Time| - " + tweet.created_at.strftime('%d/%m/%y -- %H:%M'))
 
 # Sample method, used to update a status
 # api.update_status('Test')
