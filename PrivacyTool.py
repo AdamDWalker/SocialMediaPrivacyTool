2fd46badd8a1b7a36de8c44b9eada21d656d7de3
AdamDWalker <adamwalker03@googlemail.com>
2017-02-20 13:37:29 +0000

    First Commit
    - Created python script for project
    - Installed Tweepy library
    - Created twitter application
    - Authentication through tweepy working
    - Can post to my account (not that I need to really though)

---
+++ PrivacyTool.py
@@ -0,0 +1,19 @@
+import tweepy
+
+print ("Hello World")
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = 'YUOKTebHRQH1MUv3ZlIZ3SKM3'
+consumer_secret = 'FZJtpidnF24hDL6wgTAf2Tfqa8lJj8ZaVzWLbzOAdAty7cxDFe'
+access_token = '4243760297-PPxt21hr8IZCPgYBegj4NqlHagPi5lOv2wgLv1Q'
+access_token_secret = 'Fu6m7teENyTVs92mWUt0UKriZun1vC3Ny5o1WkwpWlM8f'
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+# Sample method, used to update a status
+# api.update_status('Test')
