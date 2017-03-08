a4596d6ae41279f16ec061a27a15a876b0f52372
AdamDWalker <adamwalker03@googlemail.com>
2017-03-08 20:03:06 +0000

    Read in tweets from file
    - Analysis script now reads each tweet from the output log of the extraction script
    - \n characters in the script are stripped out too
    - Need to add checks for the information headers (e,g, Account user name, follower count...etc)
    - Minor revisions to logging format for the console

--- TweetAnalysis.py
+++ TweetAnalysis.py
@@ -1,12 +1,21 @@
 import nltk
 
-sentence = "This is the test sentence that I have written to try out various things that nltk can do."
+with open("Output_Log.txt") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+tweets = [x.strip('\n') for x in content]
 
-tokens = nltk.word_tokenize(sentence)
 
-tagged = nltk.pos_tag(tokens)
+#sentence = "@ohhitsonlyalice I've been checking your twitter so much that it comes up before my uni timetable when I type "t" into my browser :')"
+#sentence2 = "Also I won £6 on the lottery and managed to socialise too. What even is this day?!?"
+#sentence3 = "RT @SuperMickyChow: #c4debate Sheila Hancock gets biggest round of applause for preaching unity and love for a better future."
+#tokens = nltk.word_tokenize(sentence)
+
+#tagged = nltk.pos_tag(tokens)
 
 #entities = nltk.chunk.ne_chunk(sentence)
-print (tokens)
-print("\n")
-print(tagged)
+#print (tokens)
+#print("\n")
+#print(tagged)
+for tweet in tweets:
+    print(tweet + "\n")
