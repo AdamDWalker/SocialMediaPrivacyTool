dbb18495e86f2597e7577415aee3553b9b03c347
AdamDWalker <adamwalker03@googlemail.com>
2017-03-09 15:15:07 +0000

    Analysis script reads tweets from output log
    - Removed test sentences
    - Reading in tweets from the output log
    - Split these sentences based on the '~#~' ending of the string
    - Randomly select a sentence to print to the console

--- TweetAnalysis.py
+++ TweetAnalysis.py
@@ -1,14 +1,15 @@
 import nltk
+import random
 
 with open("Output_Log.txt") as f:
-    content = f.readlines()
+    tweets = f.read().split('~#~')
 # you may also want to remove whitespace characters like `\n` at the end of each line
-tweets = [x.strip('\n') for x in content]
+#tweets = [x.strip('\n') for x in content]
+for tweet in tweets:
+    tweet.strip('\n')
 
-
-#sentence = "@ohhitsonlyalice I've been checking your twitter so much that it comes up before my uni timetable when I type "t" into my browser :')"
-#sentence2 = "Also I won £6 on the lottery and managed to socialise too. What even is this day?!?"
-#sentence3 = "RT @SuperMickyChow: #c4debate Sheila Hancock gets biggest round of applause for preaching unity and love for a better future."
+rand = random.randrange(0, len(tweets))
+print(rand)
 #tokens = nltk.word_tokenize(sentence)
 
 #tagged = nltk.pos_tag(tokens)
@@ -18,4 +19,4 @@ tweets = [x.strip('\n') for x in content]
 #print("\n")
 #print(tagged)
 for tweet in tweets:
-    print(tweet + "\n")
+    print(tweets[rand] + "\n")
