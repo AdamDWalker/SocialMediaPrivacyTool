5ce123e1c81684d3625e0928ce084c9c9dde0282
AdamDWalker <adamwalker03@googlemail.com>
2017-04-01 16:33:25 +0100

    Added autorun on import protections to TweetAnalysis

--- TweetAnalysis.py
+++ TweetAnalysis.py
@@ -2,29 +2,33 @@ import nltk
 import random
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as vaderSentiment
 
-with open("Output_Log.txt") as f:
-    tweets = f.read().split('~#~')
-# you may also want to remove whitespace characters like `\n` at the end of each line
-#tweets = [x.strip('\n') for x in content]
-for tweet in tweets:
-    tweet.strip('\n')
-
-# Generate a random value so as to print a random tweet as a test sentence
-rand = random.randrange(0, len(tweets))
-
-sentence = str(tweets[rand])
-# tokens = nltk.word_tokenize(sentence)
-# tagged = nltk.pos_tag(tokens)
-# entities = nltk.chunk.ne_chunk(tagged)
-
 def getSentimentScores(sentence):
     vs = vaderSentiment()
     vsVal = vs.polarity_scores(sentence)
     return vsVal
 
+if __name__ == '__main__':
+    print("Test")
+    with open("Output_Log.txt") as f:
+        tweets = f.read().split('~#~')
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    #tweets = [x.strip('\n') for x in content]
+    for tweet in tweets:
+        tweet.strip('\n')
 
-print(sentence + "\n")
-print("\t" + str(getSentimentScores(sentence)))
+    # Generate a random value so as to print a random tweet as a test sentence
+    rand = random.randrange(0, len(tweets))
+
+    sentence = str(tweets[rand])
+    # tokens = nltk.word_tokenize(sentence)
+    # tagged = nltk.pos_tag(tokens)
+    # entities = nltk.chunk.ne_chunk(tagged)
+
+
+
+
+# print(sentence + "\n")
+# print("\t" + str(getSentimentScores(sentence)))
 # print(tokens)
 # print("\n")
 # print(tagged)
